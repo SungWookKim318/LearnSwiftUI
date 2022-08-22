ea622ad22550e55465c86603bb515d49875427a0
SungWook Kim <sungwook.kim318@gmail.com>
2022-08-22 16:40:33 +0900

    tutorial finished

--- LSU/Views/ContentView.swift
+++ LSU/Views/ContentView.swift
@@ -7,11 +7,38 @@
 
 import SwiftUI
 
+// MARK: - ContentView
+
 struct ContentView: View {
-    var body: some View {
-        LandmarkList()
+    // MARK: Internal
+
+    enum Tab {
+        case featured
+        case list
     }
+
+    var body: some View {
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
+    }
+
+    // MARK: Private
+
+    @State private var selection: Tab = .featured
 }
+
+// MARK: - ContentView_Previews
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
